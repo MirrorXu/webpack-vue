e80c851f616ac7e468fe314ba44d7dc9c03d39af
MirrorXu <xuxulee@163.com>
2017-09-22 17:51:51 +0800

    学习到60分钟左右

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# webpack-vue
-练习webpack和vue的使用结合
-# 测试
+## 
---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        hello {{ name }}
+    </div>
+
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                name:'Vue.js'
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    div{
+        color: aquamarine;
+        font-weight: bold;
+        font-family: "Times New Roman";
+        text-align: center;
+        font-size: 60px;
+    }
+</style>
